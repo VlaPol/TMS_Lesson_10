dc3129f74e1c8ed79e06dcdf0c5f4c14f497f0f5
pva <tango2002@gmail.com>
2023-02-04 16:37:53 +0300

    feat: Homework10
    First iteration

---
+++ src/by/tms/lesson10/homework/Lesson10Test.java
@@ -0,0 +1,76 @@
+package by.tms.lesson10.homework;
+
+import by.tms.lesson10.homework.utils.StringUtils;
+
+import java.util.Scanner;
+
+public class Lesson10Test {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        do {
+            System.out.println("*********** MENU *************");
+            System.out.println("1. Task_1");
+            System.out.println("2. Task_2");
+            System.out.println("3. Task_3");
+            System.out.println("4. Task_4");
+            System.out.println("5. Task_5");
+            System.out.println("6. Task_6");
+            System.out.println("0. Exit");
+            System.out.println("******************************");
+            System.out.print("Input task number (0 for exit): ");
+
+            int key = scanner.nextInt();
+            Scanner sc = new Scanner(System.in);
+
+            switch (key) {
+                case 1 -> {
+                    System.out.print("Input string: ");
+                    String inputString = sc.nextLine();
+                    System.out.print("Result: ");
+                    System.out.println(StringUtils.getStringWithoutSpareSpaces(inputString));
+                }
+                case 2 -> {
+                    System.out.print("Input card number (16 digit necessary!): ");
+                    String inputString = sc.nextLine();
+                    System.out.print("Result: ");
+                    System.out.println(StringUtils.getSecureCardNumber(inputString));
+                }
+                case 3 -> {
+                    System.out.print("Input name: ");
+                    String name = sc.nextLine();
+                    System.out.print("Input surname: ");
+                    String surname = sc.nextLine();
+                    System.out.print("Input patronymic if necessary or press enter: ");
+                    String patronymic = sc.nextLine();
+                    System.out.println(StringUtils.getSignature(name, surname, patronymic));
+                }
+                case 4 -> {
+                    System.out.print("Input passport number: ");
+                    String number = sc.nextLine();
+                    System.out.println("Are input number [" + number + "] a Belarusian passport? ->"
+                        + StringUtils.isBelarusPassport(number));
+                }
+                case 5 -> {
+                    System.out.print("Input preferred password: ");
+                    String password = sc.nextLine();
+                    System.out.println("Are this password [" + password + "] is strong? -> "
+                            + StringUtils.isStrongPassword(password));
+                }
+                case 6 -> {
+                    System.out.print("Input email address: ");
+                    String email = sc.nextLine();
+                    System.out.println("Are input string [" + email + "] is email? -> "
+                            + StringUtils.isEmail(email));
+                }
+                case 0 -> {
+                    System.out.println("See you!!!");
+                    return;
+                }
+                default -> System.out.println("Wrong key!!!");
+            }
+        } while (true);
+    }
+}
---
+++ src/by/tms/lesson10/homework/utils/StringUtils.java
@@ -0,0 +1,153 @@
+package by.tms.lesson10.homework.utils;
+public class StringUtils {
+
+    public static String getStringWithoutSpareSpaces(String inputString) {
+
+        if (inputString.length() == 0) return inputString;
+
+        String tmpString = inputString.trim();
+        if (tmpString.length() == 0) {
+            return tmpString;
+        } else {
+            char[] stringToCharArray = tmpString.toCharArray();
+
+            int elementsCounterForNewString = 1;
+
+            for (int i = 1; i < stringToCharArray.length; i++) {
+                if (!(stringToCharArray[i - 1] == ' ' && stringToCharArray[i] == ' ')) {
+                    stringToCharArray[elementsCounterForNewString++] = stringToCharArray[i];
+                }
+            }
+            return new String(stringToCharArray, 0, elementsCounterForNewString);
+        }
+    }
+
+    public static String getSecureCardNumber(String inputString) {
+
+        String tmpString = inputString.trim();
+
+        if (tmpString.length() != 16) {
+            throw new IllegalArgumentException("Bad card number!");
+        }
+
+        char[] stringArrayToChar = tmpString.toCharArray();
+        char[] secureCardNumber = new char[stringArrayToChar.length + 3];
+
+        int numberCounter = 1;
+        int meaningNumbers = 4;
+
+        for (int i = 0; i < secureCardNumber.length; i++) {
+            if (numberCounter % 5 == 0) {
+                secureCardNumber[i] = ' ';
+            } else {
+                if (i < secureCardNumber.length - 4) {
+                    secureCardNumber[i] = '*';
+                } else {
+                    secureCardNumber[i] = stringArrayToChar[stringArrayToChar.length - meaningNumbers];
+                    meaningNumbers--;
+                }
+            }
+            numberCounter++;
+        }
+        return new String(secureCardNumber);
+    }
+
+    public static String getSignature(String surname, String name, String patronymic) {
+
+        if (surname.trim().length() == 0 || name.trim().length() == 0) {
+            throw new IllegalArgumentException("Surname and name are required!");
+        }
+
+        String nameAndSurname = getPreparedString(surname) + ' ' + getPreparedString(name).charAt(0) + ".";
+
+        if (patronymic.trim().length() != 0) {
+            return nameAndSurname + ' ' + getPreparedString(patronymic).charAt(0) + '.';
+        }
+        return nameAndSurname;
+    }
+
+    public static boolean isBelarusPassport(String inputString) {
+
+        if (inputString.trim().length() != 9) {
+            return false;
+        }
+
+        char[] stringToCharArray = inputString.toCharArray();
+
+        if (!isUpperAndLatin(stringToCharArray[0]) || !isUpperAndLatin(stringToCharArray[1])) {
+            return false;
+        }
+
+        for (int i = 2; i < stringToCharArray.length; i++) {
+            if (!Character.isDigit(stringToCharArray[i])) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static boolean isStrongPassword(String inputString) {
+
+        if (inputString.length() < 8) {
+            return false;
+        }
+
+        char[] stringToChar = inputString.toCharArray();
+
+        boolean hasDigit = false;
+        boolean hasUpperLetter = false;
+        boolean hasLowerLetter = false;
+
+        for (char c : stringToChar) {
+            if (isLatinOrCyrillicUpperCase(c)) hasUpperLetter = true;
+            if (isLatinOrCyrillicLowerCase(c)) hasLowerLetter = true;
+            if (Character.isDigit(c)) hasDigit = true;
+
+            if (hasDigit && hasLowerLetter && hasUpperLetter) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean isEmail(String inputString) {
+
+        if (inputString.contains("@") && (inputString.indexOf('@') == inputString.lastIndexOf('@'))
+                && !inputString.contains(" ")) {
+            return inputString.indexOf('@') < inputString.length() - 1 && inputString.indexOf('@') > 0;
+        }
+        return false;
+    }
+
+    private static String getPreparedString(String inputString) {
+
+        String tmpString = getStringWithoutSpareSpaces(inputString);
+        char[] stringToCharArray = tmpString.toCharArray();
+
+        if (Character.isLowerCase(stringToCharArray[0])) {
+            stringToCharArray[0] = Character.toUpperCase(stringToCharArray[0]);
+            return new String(stringToCharArray);
+        }
+        return new String(stringToCharArray);
+    }
+
+    private static boolean isUpperAndLatin(char inputChar) {
+
+        return Character.isUpperCase(inputChar) && ((inputChar >= 'A' && inputChar <= 'Z') ||
+                (inputChar >= 'a' && inputChar <= 'z'));
+    }
+
+    private static boolean isLatinOrCyrillicUpperCase(char inputChar) {
+
+        return ((inputChar >= 'A' && inputChar <= 'Z') || (inputChar >= 'А' && inputChar <= 'Я')
+                || inputChar == 'Ё');
+    }
+
+    private static boolean isLatinOrCyrillicLowerCase(char inputChar) {
+
+        return ((inputChar >= 'a' && inputChar <= 'z') || (inputChar >= 'а' && inputChar <= 'я')
+                || inputChar == 'ё');
+    }
+
+}
